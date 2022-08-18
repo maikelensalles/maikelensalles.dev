72be4cf8704a21cdde232560fa4c018848fb8c55
Maikelen Salles <maiki09salles@gmail.com>
2022-08-18 04:17:58 -0300

    skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,20 @@
 import '../styles/skills.scss';
+import { FaHtml5, FaSass,
+  FaReact } from 'react-icons/fa';
+
+import {
+  SiJavascript,
+} from 'react-icons/si';
 
 export function Skills() {
     return (
-      <h1>Skills</h1>
+      <div id="skills">
+        <div className="skillsHeader">
+          <h2>Skills</h2>
+        </div>
+        <div className="skillsContainer">
+
+        </div>
+      </div>
     )
 }
