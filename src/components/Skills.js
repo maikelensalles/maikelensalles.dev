7f6fd86fa3d0b042c9c3170ee08f15dc71b75933
Maikelen Salles <maiki09salles@gmail.com>
2022-08-19 04:15:32 -0300

    hard skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,15 @@
 import '../styles/skills.scss';
-import { FaHtml5, FaSass,
-  FaReact } from 'react-icons/fa';
+import { FaHtml5, FaSass, FaReact } from 'react-icons/fa';
+import { SiJavascript, SiCss3, SiTypescript } from 'react-icons/si';  
 
-import {
-  SiJavascript,
-} from 'react-icons/si';
+const skillsData = [
+  {id: 1, title: 'HTML', icon: <FaHtml5 />},
+  {id: 2, title: 'CSS', icon: <SiCss3 />},
+  {id: 3, title: 'SASS', icon: <FaSass />},
+  {id: 4, title: 'React', icon: <FaReact />},
+  {id: 5, title: 'JavaScript', icon: <SiJavascript />},
+  {id: 6, title: 'TypeScript', icon: <SiTypescript />},
+];
 
 export function Skills() {
     return (
@@ -13,7 +18,12 @@ export function Skills() {
           <h2>Skills</h2>
         </div>
         <div className="skillsContainer">
-
+          {skillsData.map((skills) =>
+            <div className="skill--box" key={skills.id}>
+              <i className='skillsImage'>{skills.icon} </i>
+              <h3>{skills.title}</h3>
+            </div>
+          )}
         </div>
       </div>
     )
