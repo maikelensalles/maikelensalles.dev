e80d1aead08a80c711ad158bd0f59c51b61e3be6
Maikelen Salles <maiki09salles@gmail.com>
2022-08-23 23:17:18 -0300

    projetos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,13 @@
 import '../styles/projects.scss';
+import { FaCode } from 'react-icons/fa';
 
 const projectData = [
-  {id: 1, title: 'Sistema para gerenciamento de aplicativo P2P', image: 'https://maikelensalles.github.io/site/images/photography-4.jpg'},
-  {id: 2, title: 'Layout de sistema desenvolvido com HTML, CSS e SCSS', image: 'https://maikelensalles.github.io/site/images/photography-1.jpg'},
-  {id: 3, title: 'Sistema de gerenciamento de investimentos', image: 'https://maikelensalles.github.io/site/images/photography-2.jpg'},
+  {id: 1, title: 'Sistema para gerenciamento de aplicativo P2P', image: 'https://maikelensalles.github.io/site/images/photography-4.jpg', 
+   code: 'https://github.com/maikelensalles/forte-play', demo: 'https://github.com/maikelensalles/forte-play', tags: ['JavaScript', 'CSS', 'SASS', 'Laravel']},
+  {id: 2, title: 'Sistema para gerenciamento de delivery', image: 'https://maikelensalles.github.io/site/images/photography-3.jpg',
+   code: 'https://github.com/maikelensalles/delivery', demo: 'https://github.com/maikelensalles/delivery', tags: ['Laravel', 'Blade', 'Less']},
+  {id: 3, title: 'Sistema para gerenciamento de investimentos', image: 'https://maikelensalles.github.io/site/images/photography-2.jpg',
+   code: 'https://github.com/maikelensalles/ativos-master', demo: 'https://github.com/maikelensalles/ativos-master', tags: ['Blade', 'PHP', 'Laravel']},
 ];
 
 export function Projects() {
@@ -14,12 +18,30 @@ export function Projects() {
         </div>
         <div className="projects--body">
           <div className="projects--bodyContainer">
-            {projectData.map((project) =>
-              <div className="project--box" key={project.id}>
-                <h3>{project.title}</h3>
-                <img src={project.image} />
+            {projectData.slice(0, 3).map(project => (
+              <div className="singleProject" key={project.id}>
+                <div className="projectContent">
+                  <h2>{project.title}</h2>
+                  <img src={project.image} />
+                  <div className='project--showcaseBtn'>
+                      <a
+                          href={project.code}
+                          target='_blank'
+                          rel='noreferrer'
+                      >
+                          <FaCode
+                            className='icon'
+                          />
+                      </a>
+                  </div>
+                </div>
+                <div className='project--lang'>
+                {project.tags.map((tag, id) => (
+                        <span key={id}>{tag}</span>
+                    ))}
+                </div>
               </div>
-            )}
+            ))}
           </div>
         </div>
       </div>    
