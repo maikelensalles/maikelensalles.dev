96717111b498929e801013db9092813e1b409501
Maikelen Salles <maiki09salles@gmail.com>
2022-08-24 19:30:24 -0300

    novos projetos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,18 @@ import '../styles/projects.scss';
 import { FaCode } from 'react-icons/fa';
 
 const projectData = [
-  {id: 1, title: 'Sistema para gerenciamento de aplicativo P2P', image: 'https://maikelensalles.github.io/site/images/photography-4.jpg', 
-   code: 'https://github.com/maikelensalles/forte-play', demo: 'https://github.com/maikelensalles/forte-play', tags: ['JavaScript', 'CSS', 'SASS', 'Laravel']},
-  {id: 2, title: 'Sistema para gerenciamento de delivery', image: 'https://maikelensalles.github.io/site/images/photography-3.jpg',
-   code: 'https://github.com/maikelensalles/delivery', demo: 'https://github.com/maikelensalles/delivery', tags: ['Laravel', 'Blade', 'Less']},
-  {id: 3, title: 'Sistema para gerenciamento de investimentos', image: 'https://maikelensalles.github.io/site/images/photography-2.jpg',
-   code: 'https://github.com/maikelensalles/ativos-master', demo: 'https://github.com/maikelensalles/ativos-master', tags: ['Blade', 'PHP', 'Laravel']},
+  {id: 1, title: 'Sistema de salas', image: 'https://maikelensalles.site/images/letmeask-reactjs.jpg', 
+   code: 'https://github.com/maikelensalles/Letmeask-ReactJS', tags: ['TypeScript', 'SASS', 'HTML']},
+  {id: 2, title: 'Landing Page', image: 'https://maikelensalles.site/images/landing-page-meus-links.jpg', 
+   code: 'https://github.com/maikelensalles/Landing-page---Meus-Links', tags: ['HTML', 'CSS']},
+  {id: 3, title: 'Layout de sistema', image: 'https://maikelensalles.site/images/ui-dashboard.jpg', 
+   code: 'https://github.com/maikelensalles/UI-Dashboard', tags: ['HTML', 'CSS', 'SASS', 'Bootstrap']},
+  {id: 4, title: 'Sistema para gerenciamento de aplicativo P2P', image: 'https://maikelensalles.github.io/site/images/photography-4.jpg', 
+   code: 'https://github.com/maikelensalles/forte-play', tags: ['JavaScript', 'CSS', 'SASS', 'Laravel']},
+  {id: 5, title: 'Sistema para gerenciamento de delivery', image: 'https://maikelensalles.github.io/site/images/photography-3.jpg',
+   code: 'https://github.com/maikelensalles/delivery', tags: ['Laravel', 'Blade', 'Less']},
+  {id: 6, title: 'Sistema para gerenciamento de investimentos', image: 'https://maikelensalles.github.io/site/images/photography-2.jpg',
+   code: 'https://github.com/maikelensalles/ativos-master', tags: ['Blade', 'PHP', 'Laravel']}
 ];
 
 export function Projects() {
@@ -18,7 +24,7 @@ export function Projects() {
         </div>
         <div className="projects--body">
           <div className="projects--bodyContainer">
-            {projectData.slice(0, 3).map(project => (
+            {projectData.slice(0, 6).map(project => (
               <div className="singleProject" key={project.id}>
                 <div className="projectContent">
                   <h2>{project.title}</h2>
