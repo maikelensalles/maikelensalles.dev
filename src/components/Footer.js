47bb74a55de9437d05e3bf77c7ff005c1eb8e007
Maikelen Salles <maiki09salles@gmail.com>
2022-08-25 00:30:26 -0300

    minha logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 import '../styles/footer.scss';
 
+const imagem = ['https://maikelensalles.site/images/maikelensalles..png']
+
 export function Footer() {
     return (
       <div id="footer">
+        <img src={imagem} />
         <p>Made with ♡ by Maikelen Salles</p>
       </div>
     )
