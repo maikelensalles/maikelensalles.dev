a8d0f77fd6b810ac19393ce19c12abafea491ec7
Maikelen Salles <maiki09salles@gmail.com>
2022-08-31 13:38:31 -0300

    organizei componentes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import '../styles/header.scss';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { SiWhatsapp, SiInstagram } from 'react-icons/si';
 
+const images = ['https://maikelensalles.site/images/maikelensalles2.png']
+
 const headerData = {
   name: 'Maikelen Salles',
   title: "Web Developer",
@@ -20,104 +22,48 @@ const socialsData = {
 export function Header() {
 
     return (
-      <div className='landing'>
-          <div className='landing--container'>
-          <div className='landing--container-left'>
-              <div className='lcl--content'>
-                  {socialsData.linkedIn && (
-                      <a
-                          href={socialsData.linkedIn}
-                          target='_blank'
-                          rel='noreferrer'
-                      >
-                          <FaLinkedin
-                              className='landing--social'
-                              aria-label='LinkedIn'
-                          />
-                      </a>
-                  )}
-                  {socialsData.github && (
-                      <a
-                          href={socialsData.github}
-                          target='_blank'
-                          rel='noreferrer'
-                      >
-                          <FaGithub
-                              className='landing--social'
-                              aria-label='GitHub'
-                          />
-                      </a>
-                  )}
-                  {socialsData.whatsapp && (
-                      <a
-                          href={socialsData.whatsapp}
-                          target='_blank'
-                          rel='noreferrer'
-                      >
-                          <SiWhatsapp
-                              className='landing--social'
-                              aria-label='Whatsapp'
-                          />
-                      </a>
-                  )}
-                  {socialsData.instagram && (
-                      <a
-                          href={socialsData.instagram}
-                          target='_blank'
-                          rel='noreferrer'
-                      >
-                          <SiInstagram
-                              className='landing--social'
-                              aria-label='Instagram'
-                          />
-                      </a>
-                  )}
-              </div>
-          </div>
-              <img
-                  src={headerData.image}
-                  alt=''
-                  className='landing--img'
-              />
-              <div
-                    className='landing--container-right'
-                >
-                  <div className='lcl--container'>
-                      {socialsData.linkedIn && (
-                          <a
-                              href={socialsData.linkedIn}
-                              target='_blank'
-                              rel='noreferrer'
-                          >
-                              <FaLinkedin
-                                  className='duol--social'
-                                  aria-label='LinkedIn'
-                              />
-                          </a>
-                      )}
-                      {socialsData.github && (
-                          <a
-                              href={socialsData.github}
-                              target='_blank'
-                              rel='noreferrer'
-                          >
-                              <FaGithub
-                                  className='duol--social'
-                                  aria-label='GitHub'
-                              />
-                          </a>
-                      )}
-                      {socialsData.whatsapp && (
-                      <a
-                          href={socialsData.whatsapp}
-                          target='_blank'
-                          rel='noreferrer'
-                      >
-                          <SiWhatsapp
-                              className='duol--social'
-                              aria-label='Whatsapp'
-                          />
-                      </a>
+    <div id="header">
+        <div className="navbar--container">
+        </div>
+        <div className='landing'>
+            <div className='landing--container'>
+            <div className='landing--container-left'>
+                <div className='lcl--content'>
+                    {socialsData.linkedIn && (
+                        <a
+                            href={socialsData.linkedIn}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <FaLinkedin
+                                className='landing--social'
+                                aria-label='LinkedIn'
+                            />
+                        </a>
+                    )}
+                    {socialsData.github && (
+                        <a
+                            href={socialsData.github}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <FaGithub
+                                className='landing--social'
+                                aria-label='GitHub'
+                            />
+                        </a>
+                    )}
+                    {socialsData.whatsapp && (
+                        <a
+                            href={socialsData.whatsapp}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <SiWhatsapp
+                                className='landing--social'
+                                aria-label='Whatsapp'
+                            />
+                        </a>
                     )}
                     {socialsData.instagram && (
                         <a
@@ -126,21 +72,81 @@ export function Header() {
                             rel='noreferrer'
                         >
                             <SiInstagram
-                                className='duol--social'
+                                className='landing--social'
                                 aria-label='Instagram'
                             />
                         </a>
                     )}
-                  </div>
-                  <div
-                      className='lcr--content'
-                  >
-                      <h6>{headerData.title}</h6>
-                      <h1>{headerData.name}</h1>
-                      <p>{headerData.desciption}</p>                      
-                  </div>
-              </div>
-          </div>
-      </div>
+                </div>
+            </div>
+                <img
+                    src={headerData.image}
+                    alt=''
+                    className='landing--img'
+                />
+                <div
+                        className='landing--container-right'
+                    >
+                    <div className='lcl--container'>
+                        {socialsData.linkedIn && (
+                            <a
+                                href={socialsData.linkedIn}
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <FaLinkedin
+                                    className='duol--social'
+                                    aria-label='LinkedIn'
+                                />
+                            </a>
+                        )}
+                        {socialsData.github && (
+                            <a
+                                href={socialsData.github}
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <FaGithub
+                                    className='duol--social'
+                                    aria-label='GitHub'
+                                />
+                            </a>
+                        )}
+                        {socialsData.whatsapp && (
+                        <a
+                            href={socialsData.whatsapp}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <SiWhatsapp
+                                className='duol--social'
+                                aria-label='Whatsapp'
+                            />
+                        </a>
+                        )}
+                        {socialsData.instagram && (
+                            <a
+                                href={socialsData.instagram}
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <SiInstagram
+                                    className='duol--social'
+                                    aria-label='Instagram'
+                                />
+                            </a>
+                        )}
+                    </div>
+                    <div
+                        className='lcr--content'
+                    >
+                        <h6>{headerData.title}</h6>
+                        <h1>{headerData.name}</h1>
+                        <p>{headerData.desciption}</p>                      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     )
 }
