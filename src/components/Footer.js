a7dec7e0e67b497e6dda7a9c058d8d8c433e9cda
Maikelen Salles <maiki09salles@gmail.com>
2022-09-01 03:04:57 -0300

    img alt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const imagem = ['https://maikelensalles.site/images/maikelensalles..png']
 export function Footer() {
     return (
       <div id="footer">
-        <img src={imagem} />
+        <img src={imagem} alt='' />
         <p>Made with ♡ by Maikelen Salles</p>
       </div>
     )
