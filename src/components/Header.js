8ef79d6ab3de7f58765effb8975eea49c3e33460
Maikelen Salles <maiki09salles@gmail.com>
2022-09-03 02:05:30 -0300

    descrição

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const images = ['https://maikelensalles.site/images/maikelensalles2.png']
 const headerData = {
   name: 'Maikelen Salles',
   title: "Web Developer",
-  desciption:"Mantenho interesse pela área de desenvolvimento front-end. Na minha experiência, sempre foquei em boas práticas no desenvolvimento de cada projeto e estou disposta a aprender muito mais. Algumas palavras-chave que fazem parte da minha rotina como desenvolvedora: modelo de desenvolvimento ágil, trabalho em equipe, Clean Code, princípios SOLID, consumo de APIs, versionamento com git. Aguardem as cenas dos próximos capítulos dessa jornada ;)",
+  desciption:"Mantenho interesse pela área de desenvolvimento front-end. Na minha experiência, sempre foquei em boas práticas no desenvolvimento de cada projeto e estou disposta a aprender muito mais. Algumas palavras-chave que fazem parte da minha rotina como desenvolvedora: modelo de desenvolvimento ágil, acessibilidade, trabalho em equipe, Clean Code, princípios SOLID, Figma, HTML5, CSS3, SASS, JavaScript, React, Node.js, WordPress, consumo de APIs, versionamento com git.",
   image: 'https://avatars.githubusercontent.com/u/68317244?v=4',
 }
 
