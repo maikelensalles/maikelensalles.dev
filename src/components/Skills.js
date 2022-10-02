83047ee79ddf145228e04ea3f33d64e279352c18
Maikelen Salles <maiki09salles@gmail.com>
2022-10-02 01:57:29 -0300

    skills web design

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,22 @@
 import '../styles/skills.scss';
-import { FaHtml5, FaSass, FaReact, FaFigma, FaLinux } from 'react-icons/fa';
-import { SiJavascript, SiCss3, SiTypescript, SiFirebase, SiGit } from 'react-icons/si';
+import { FaHtml5, FaSass, FaReact, FaFigma, FaLinux, FaWordpress, FaLaravel } from 'react-icons/fa';
+import { SiJavascript, SiCss3, SiTypescript, SiFirebase, SiGit, SiAdobephotoshop, SiBootstrap } from 'react-icons/si';
 
 const skillsData = [
   {id: 1, title: 'HTML', icon: <FaHtml5 />},
   {id: 2, title: 'CSS', icon: <SiCss3 />},
   {id: 3, title: 'SASS', icon: <FaSass />},
-  {id: 4, title: 'React', icon: <FaReact />},
-  {id: 5, title: 'JavaScript', icon: <SiJavascript />},
-  {id: 6, title: 'TypeScript', icon: <SiTypescript />},
+  {id: 4, title: 'Bootstrap', icon: <SiBootstrap />},
+  {id: 5, title: 'React', icon: <FaReact />},
+  {id: 6, title: 'JavaScript', icon: <SiJavascript />},
+  {id: 7, title: 'TypeScript', icon: <SiTypescript />},
   {id: 8, title: 'Git', icon: <SiGit />},
   {id: 9, title: 'Figma', icon: <FaFigma />},
   {id: 10, title: 'Linux', icon: <FaLinux />},
-  {id: 10, title: 'Firebase', icon: <SiFirebase />},
+  {id: 11, title: 'Firebase', icon: <SiFirebase />},
+  {id: 12, title: 'WordPress', icon: <FaWordpress />},
+  {id: 13, title: 'Photoshop', icon: <SiAdobephotoshop />},
+  {id: 14, title: 'Laravel', icon: <FaLaravel />},
 ];
 
 export function Skills() {
